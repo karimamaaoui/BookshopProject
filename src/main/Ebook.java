16e1d8a146aa1c8a1a3fba18a89e81add3ecff8a
hayet <hayetchemkhi1994@gmail.com>
2023-04-19 23:16:07 +0200

    first

--- src/main/Ebook.java
+++ src/main/Ebook.java
@@ -25,9 +25,6 @@ public class Ebook extends Application {
         primaryStage.show();}
    
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
